7394d0177724d15d59a72a4b4e8438dfed5cbb8e
Xlence1624 <xlence1624@gmail.com>
2026-04-28 15:10:07 +0100

    Employee card created
    Signed-off-by: Xlence1624 <xlence1624@gmail.com>

--- Client/src/components/EmployeeCard.jsx
+++ Client/src/components/EmployeeCard.jsx
@@ -3,13 +3,25 @@ import React from 'react'
 const EmployeeCard = ({employee, onDelete, onEdit}) => {
   return (
     <div className='group relative card card-hover overflow-hidden'> 
-<div >
 
+<div className='relative aspect-4/3 w-full overflow-hidden bg-linear-to-br from-slate-100 to-slate-50'>
+<div className='w-full h-full flex items-center justify-center'>
+  {/* Circle icon */}
+<div className='w-20 h-20 rounded-full bg-linear-to-br from-indigo-100 to-slate-100 flex items-center justify-center'>
+<span className='text-2xl font-medium text-indigo-400'>
+  {employee.firstName[0]} {employee.lastName[0]}
+</span>
 </div>
+</div>
+</div>
+<div className='absolute top-3 left-3 flex gap-2'>
+<span className='bg-white/90 backdrop-blur-sm px-2.5 py-1 text-xs font-semibold text-slate-600 rounded-lg shadow-sm' >{employee.department || "Remote"}</span>
+</div>
+
 
 <div className='p-5'>
   <h3>{employee.firstName}  {employee.lastName}</h3>
-  <p className='text-xs text-slate-500'></p>
+  <p className='text-xs text-slate-500'>{employee.position}</p>
   </div>
       
   </div>
--- Client/src/pages/Employees.jsx
+++ Client/src/pages/Employees.jsx
@@ -1,85 +1,104 @@
-import React, { useEffect, useState } from 'react'
-import { useCallback } from 'react'
-import { dummyEmployeeData, DEPARTMENTS } from '../assets/assets'
-import { Plus, Search } from 'lucide-react'
-import EmployeeCard from '../components/EmployeeCard'
+import React, { useEffect, useState } from "react";
+import { useCallback } from "react";
+import { dummyEmployeeData, DEPARTMENTS } from "../assets/assets";
+import { Plus, Search } from "lucide-react";
+import EmployeeCard from "../components/EmployeeCard";
 
 const Employees = () => {
+  const [employees, setEmployees] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [search, setsearch] = useState("");
+  const [selectedDept, setSelectedDept] = useState("");
+  const [editEmployee, setEditEmployee] = useState(null);
+  // cosnt [showCreateModel, setShowCreateModel] = useState(false)
+  const fetchEmployees = useCallback(async () => {
+    setLoading(true);
+    setEmployees(
+      dummyEmployeeData.filter((emp) =>
+        selectedDept ? emp.department === selectedDept : emp,
+      ),
+    );
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
 
-  const [employees, setEmployees] = useState([])
-  const [loading, setLoading] = useState(true)
-  const fetchEmployees = useCallback( 
-async () => {
-  setLoading(true)
-  setEmployees(dummyEmployeeData.filter((emp) => (selectedDept ? emp.department === selectedDept : emp)))
-  setTimeout(
-    ()=>{
-      setLoading(false)
-    }, 1000
-  )
-}, []
+  useEffect(() => {
+    fetchEmployees();
+  }, []);
 
-  )
-
-  useEffect(
-    ()=>{fetchEmployees()},[]
-  )
-
-  const [search, setsearch] = useState("")
-    const [selectedDept, setSelectedDept] = useState("")
-    const filtered = employees.filter(
-      (emp) => `${emp.firstName} ${emp.lastName} ${emp.position}`.toLowerCase().includes(search.toLowerCase())
-    )
+  const filtered = employees.filter((emp) =>
+    `${emp.firstName} ${emp.lastName} ${emp.position}`
+      .toLowerCase()
+      .includes(search.toLowerCase()),
+  );
   return (
-    <div className='animate-fade-in'>
+    <div className="animate-fade-in">
+      {/* Header */}
+      <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4 mb-8">
+        <div>
+          <h1 className="page-title">Employees</h1>
+          <p className="page-subtitle">Manage your team membebrs</p>
+        </div>
+        <button className="btn-primary flex items-center gap-2 w-full sm:w-auto justify-center">
+          <Plus size={16} /> Add Employee
+        </button>
+      </div>
 
-{/* Header */}
-<div className='flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4 mb-8'>
-<div>
-<h1 className='page-title'>Employees</h1>
-<p className='page-subtitle'>Manage your team membebrs</p>
+      {/* Search bar */}
 
-</div>
-<button className='btn-primary flex items-center gap-2 w-full sm:w-auto justify-center'>
-  <Plus size={16}/> Add Employee
-</button>
-
-
-</div>
-
-{/* Search bar */}
-
-<div className='flex flex-col sm:flex-row gap-3 mb-6'> 
-<div className='relative flex-1'>
-<Search className='absolute left-3.5 top-1/2 transform -translate-y-1/2 text-slate-400 w-4 h-4'/>
-<input type="text" name="" id="" placeholder='Search employee' className='w-full pl-10!' onChange={(e) =>setsearch(e.target.value)} value={search}/>
-</div>
-<select value={selectedDept} onChange={(e) => setSelectedDept(e.targetvalue)} className='max-w-40'>
-  <option value="">All Departments</option>
-  {DEPARTMENTS.map(
-    (deptname) => (
-      <option value={deptname}  key={deptname}>{deptname}</option>
-    )
-  )}
-</select>
-</div>
-{/* Employee card */}
-{loading ? (<div className='flex justify-center p-12'>
-  <div className='animate-spin h-8 w-8 border-2 border-indigo-600 border-t-transparent rounded-full'/>
-</div>) : (<div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 sm-gap-5'>
-{filtered.length === 0 ? (
-  <p className='col-span-full text-center py-16 text-slate-400 bg-white rounded-2xl border  border-dashed border-slate-200'>  No employee found</p> 
-)  : ( 
-filtered.map(
-  (emp)=> <EmployeeCard key={emp.id} employee={emp} onDelete={fetchEmployees} onEdit={}/>
-)
-
-)}
-
-</div>) }
-
+      <div className="flex flex-col sm:flex-row gap-3 mb-6">
+        <div className="relative flex-1">
+          <Search className="absolute left-3.5 top-1/2 transform -translate-y-1/2 text-slate-400 w-4 h-4" />
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="Search employee"
+            className="w-full pl-10!"
+            onChange={(e) => setsearch(e.target.value)}
+            value={search}
+          />
+        </div>
+        <select
+          value={selectedDept}
+          onChange={(e) => setSelectedDept(e.targetvalue)}
+          className="max-w-40"
+        >
+          <option value="">All Departments</option>
+          {DEPARTMENTS.map((deptname) => (
+            <option value={deptname} key={deptname}>
+              {deptname}
+            </option>
+          ))}
+        </select>
+      </div>
+      {/* Employee card */}
+      {loading ? (
+        <div className="flex justify-center p-12">
+          <div className="animate-spin h-8 w-8 border-2 border-indigo-600 border-t-transparent rounded-full" />
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 sm-gap-5">
+          {filtered.length === 0 ? (
+            <p className="col-span-full text-center py-16 text-slate-400 bg-white rounded-2xl border  border-dashed border-slate-200">
+              {" "}
+              No employee found
+            </p>
+          ) : (
+            filtered.map((emp) => (
+              <EmployeeCard
+                key={emp.id}
+                employee={emp}
+                onDelete={fetchEmployees}
+                onEdit={(e) => setEditEmployee(e)}
+              />
+            ))
+          )}
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Employees
+export default Employees;
