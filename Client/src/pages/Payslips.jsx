2bf20e2cd6c564444e9401e8720fe16a36e94363
Xlence1624 <xlence1624@gmail.com>
2026-05-16 14:49:13 +0100

    payslip ui started
    Signed-off-by: Xlence1624 <xlence1624@gmail.com>

--- Client/src/pages/Payslips.jsx
+++ Client/src/pages/Payslips.jsx
@@ -1,8 +1,58 @@
 import React from 'react'
+import { dummyEmployeeData, dummyPayslipData } from '../assets/assets'
+import { useState, useEffect, useCallback } from 'react'
+import Loading from '../components/Loading';
 
 const Payslips = () => {
+  const [payslips, setPayslips] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [employee, setEmployee] = useState([]);
+  const isAdmin = true;
+
+  const fetchPayslips = useCallback( async () => {
+      setPayslips(dummyPayslipData);  
+      setTimeout(
+        () => {
+          setLoading(false);
+        }
+      ,1000)
+    }, []
+  );
+
+  useEffect(
+    () => {
+      fetchPayslips();
+    }, [fetchPayslips]
+  );
+    useEffect(
+    () => {
+    if(isAdmin){
+      setEmployee(dummyEmployeeData);
+    }
+  }, [isAdmin]
+  );
+if (loading) return <Loading/>
   return (
-    <div>Payslips</div>
+    <div className="animate-fade-in">
+
+<div className="flex flex-col sm:flex-row justify-between items-center gap-4 mb-8">
+
+<div className="">
+
+  <h1 className="page-title"> Payslips</h1>
+  <p className="page-subtitle">
+    {isAdmin? "View and manage employee payslips" : "Your monthly payslips and salary details"}
+  </p>
+</div>
+
+{isAdmin && <p>Generate Form</p>}
+
+</div>
+
+<p className="page-description">Payslip list</p>
+
+
+    </div>
   )
 }
 
